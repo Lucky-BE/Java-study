0e4c4c2c7966a4e0c5d6f1bfe099f9905027cfd5
Lucky-BE <79445611+Lucky-BE@users.noreply.github.com>
2021-07-07 00:17:42 +0900

    Update A0701.java

--- A0701.java
+++ A0701.java
@@ -1,29 +1,51 @@
-// 배열안의 수중 최댓값 출력하기
-
 import java.util.Scanner;
 
-public class A0701_1 {
-	public static int big(int a[]) {
-		int max=0;
-		
-		for(int n:a) {
-			if(n>max) {
-				max=n;
-			}
-		}return max;
-	}
+public class A0701 {
 	
 	public static void main(String[] args) {
 	
-		int a[]=new int[7];
+		//정수 7개 입력해서 가장 큰 수 출력
+		/*
 		Scanner s=new Scanner(System.in);
+		int a,large=0;
 		
-		System.out.println("정수 7개 입력: ");
-		for(int i=0;i<a.length;i++) {
-			
-			a[i]=s.nextInt();
+		for(int i=1;i<=7;i++) {
+			a=s.nextInt();
+			if(a>large) {
+				large=a;
+			}
+		}System.out.println("가장 큰 값 : "+large);
+		
+		int b[]=new int[7];
+		int big=0;
+		
+		for(int i=0;i<b.length;i++) {
+			b[i]=s.nextInt();
+			if(big<b[i]) {
+				big=b[i];
+			}
 		}
-		System.out.println(big(a)); // 매개변수가 배열명일때, 호출할때 배열명만 적음
+		System.out.println(big);
+		*/
+		
+		
+		// 문자열 거꾸로 출력하기
+		/*
+		Scanner s=new Scanner(System.in);
+		String str;
+		
+		System.out.println("글자 입력");
+		str=s.nextLine();
+		
+		System.out.println("입력한 문자열: "+str);
+		
+		int cnt=.length();
+		for(int i=cnt-1;i>=0;i--) {
+			System.out.print(str.charAt(i));
+		}
+		*/
+		
+	
+		
 	}
-
 }
